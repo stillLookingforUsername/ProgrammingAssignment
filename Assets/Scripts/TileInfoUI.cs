c519930fd50baea452ebb4a69ca684ce5a0074c9
bankimRabha <144623987+stillLookingforUsername@users.noreply.github.com>
2025-12-14 23:52:23 +0530

    TileInfoUI

--- Assets/Scripts/TileInfoUI.cs
+++ Assets/Scripts/TileInfoUI.cs
@@ -1,20 +1,39 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TileInfoUI : MonoBehaviour
 {
     public TextMeshProUGUI tileInfoText;
+    private PlayerInputActions _playerInputActions;
+    private Camera _mainCamera;
+
+    private void Awake()
+    {
+        _playerInputActions = new PlayerInputActions();
+        _mainCamera = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        _playerInputActions.Enable();
+    }
+    private void OnDisable()
+    {
+        _playerInputActions.Disable();
+    }
 
     private void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector2 mousePos = _playerInputActions.Player.Point.ReadValue<Vector2>();
+        Ray ray = _mainCamera.ScreenPointToRay(mousePos);
 
         if(Physics.Raycast(ray, out RaycastHit hitInfo))
         {
             GridTile tile = hitInfo.collider.GetComponent<GridTile>();
             if(tile != null)
             {
-                tileInfoText.text = $"Tile Position: ({tile.x}, {tile.y})";
+                tileInfoText.text = $"Tile Position: ({tile.x}, {tile.y})\n";
             }
         }
     }
