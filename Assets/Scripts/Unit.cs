771012176ee453b5a5d51deebd01b96d300238bb
bankimRabha <144623987+stillLookingforUsername@users.noreply.github.com>
2025-12-16 16:33:33 +0530

    FaceMovement Direction

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -10,32 +10,85 @@ public class Unit : MonoBehaviour
     public bool isMoving;
 
     //[Header("Animation")]
-    //protected Animator animator;
+    //[SerializeField] protected Animator animator;
 
 /*
     protected virtual void Awake()
     {
         if (animator == null)
         {
-            animator = GetComponent<Animator>();
+            animator = GetComponentInChildren<Animator>();
         }
     }
     */
 
+    #region ToRoateCharacter Towards the direction
+    [SerializeField] protected float turnSpeed = 10f;
+
+    protected void RotateCharacter(Vector3 direction)
+    {
+        if(direction == Vector3.zero)
+        {
+            return;
+        }
+        Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.Slerp(
+        transform.rotation,
+        targetRotation,
+        Time.deltaTime * turnSpeed
+        );
+    }
+    #endregion
+
     public IEnumerator Move(List<GridTile> path)
     {
         isMoving = true;
+        //animator.SetBool("isMovingAnimatorParam", true);
+        //animator.SetBool("isMovingAnimatorParam", isMoving);
+        /*
         foreach(GridTile tile in path)
         {
             isMoving = true;
-            //animator.SetBool("isMovingAnimatorParam", true);
 
+            //snap to tile offset for y-axis
             transform.position = tile.WorldPosition + Vector3.up * characterAboveGround;
             currentTile = tile;
+            //endregion
+
             //PositionCharacterToTile(tile);
             yield return new WaitForSeconds(0.3f);
         }
+        */
+         foreach (GridTile tile in path)
+        {
+        Vector3 startPos = transform.position;
+        Vector3 endPos = tile.WorldPosition + Vector3.up * characterAboveGround;
+
+        Vector3 moveDir = (endPos - startPos).normalized;
+
+        float t = 0f;
+        float moveDuration = 0.25f;
+
+        while (t < 1f)
+        {
+            RotateCharacter(moveDir);
+
+            transform.position = Vector3.Lerp(startPos, endPos, t);
+            t += Time.deltaTime / moveDuration;
+            yield return null;
+        }
+
+        // Snap to exact tile position
+        transform.position = endPos;
+
+        currentTile = tile;
+        yield return new WaitForSeconds(0.3f);
+        }
+
+
         isMoving = false;
+        //animator.SetBool("isMovingAnimatorParam", false);
+        //animator.SetBool("isMovingAnimatorParam", isMoving);
         //animator.SetBool("isMovingAnimatorParam", false);
     }
 }
