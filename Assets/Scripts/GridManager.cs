67e479217d4a98b368dbdecfbfec7095fe8cc602
bankimRabha <144623987+stillLookingforUsername@users.noreply.github.com>
2025-12-16 17:27:31 +0530

    Refactor GridManager

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 
+//this script allows to create as well as to access the every grid
 public class GridManager : MonoBehaviour
 {
     public static GridManager Instance;
 
-    public int tileSize = 2;
-    public int gridWidth = 10;
-    public int gridHeight = 10;
+    public float tileSize = 2.2f; //gap between each tile(in world space) not the actual size of each tile or grid 
+    public int gridWidth = 10;  //no of tiles on x-axis
+    public int gridHeight = 10; //no of tiles on y-axis
     public GameObject tilePrefab;
 
-    public GridTile[,] grid;
+    public GridTile[,] grid;    //using array to to store all the spawned tiles
 
     private void Awake()
     {
@@ -22,6 +23,7 @@ public class GridManager : MonoBehaviour
         GenerateGrid();
     }
     
+    //function to spawn tiles with respect to their position
     private void GenerateGrid()
     {
         grid = new GridTile[gridWidth, gridHeight];
@@ -30,8 +32,10 @@ public class GridManager : MonoBehaviour
         {
             for (int y = 0; y < gridHeight; y++)
             {
-                GameObject tile = Instantiate(tilePrefab, new Vector3(x*tileSize, 0, y*tileSize), Quaternion.identity,transform);
-                tile.name = $"Tile_{x}_{y}";
+                Vector3 worldPos = new Vector3(x*tileSize,0,y*tileSize);
+                GameObject tile = Instantiate(tilePrefab, worldPos, Quaternion.identity,transform);
+
+                //to store the co-ordinates of x and y to use it later
                 GridTile gridTile = tile.GetComponent<GridTile>();
                 gridTile.x = x;
                 gridTile.y = y;
@@ -40,8 +44,11 @@ public class GridManager : MonoBehaviour
         }
     }
 
+    //function to get tile at a give position
     public GridTile GetTileAtPosition(int x, int y)
     {
+        //statement to check whether a gile exist or not at a given x and y 
+        //if not return null 
         if (x<0 || y<0 || x>=gridWidth || y>=gridHeight)
         {
             return null;
